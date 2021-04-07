782113bdec60750e2a171d46c3d798f0e821e32b
malcops <paul.malcolm@protonmail.com>
2021-04-07 08:33:48 -0400

    Add pytest to pyproject.toml, Initial test skeleton

---
+++ h6/test_zoom_h6.py
@@ -0,0 +1,46 @@
+from . import ZoomH6
+
+class TestZoomH6:
+
+    def test_initialization(self):
+        pass
+
+    def test_stop(self):
+        pass
+
+    def test_forward(self):
+        pass
+
+    def test_reverse(self):
+        pass
+
+    def test_play_pause(self):
+        pass
+
+    def test_record(self):
+        pass
+
+    def test_volume(self):
+        pass
+
+    def test_ch4(self):
+        pass
+
+    def test_ch3(self):
+        pass
+
+    def test_ch2(self):
+        pass
+
+    def test_ch1(self):
+        pass
+
+    def test_chr(self):
+        pass
+
+    def test_chl(self):
+        pass
+
+    def test_release_button(self):
+        pass
+
